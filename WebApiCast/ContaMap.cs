3d8c26e14923d233c061bf031e1962008e9902f2
Maurício Torquato Lopes <mauricio.torquato.lopes@gmail.com>
2023-10-17 01:20:10 -0300

    incluindo a entidade de conta, mapeamento e o crud

---
+++ WebApiCast/ContaMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using WebApiCast.Entities;
+
+namespace WebApiCast
+{
+    public class ContaMap : IEntityTypeConfiguration<Conta>
+    {
+        public void Configure(EntityTypeBuilder<Conta> builder)
+        {
+            builder.ToTable("Conta");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome).HasColumnType("varchar(200)");
+            builder.Property(x => x.Descricao).HasColumnType("nvarchar(255)");
+        }
+    }
+}
